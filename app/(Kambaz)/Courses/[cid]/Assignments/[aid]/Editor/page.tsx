e9864e41847096d2ed36bf4e302c0e288a78ac97
JerryYanTY <yan.tianyu@northeastern.edu>
2025-10-03 03:05:39 +0800

    Update page.tsx

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/Editor/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/Editor/page.tsx
@@ -1,10 +1,17 @@
 import AssignmentEditor from "./Editor";
-
+import  Button  from "react-bootstrap/Button";
 export default function Editor() {
     return (
    <div id ="wd-editor">
     <h1>Assignment Editor</h1>
         <AssignmentEditor/>
+        <hr className="my-4">
+        </hr>
+        <div className="d-flex justify-content-end gap-2">
+            <Button variant="light">Cancel</Button>
+            <Button variant="danger">Save</Button>
+        </div>
    </div>
+
     );
 }
