2f18a9d407e7d03eab70cfa223f7a3700fd3e48c
JerryYanTY <yan.tianyu@northeastern.edu>
2025-10-02 17:56:28 +0800

    Course page

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 import { Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import {CardBody, CardImg,CardText, CardTitle, Card} from "react-bootstrap";
@@ -12,10 +11,10 @@ export default function Dashboard() {
         <Row xs={1} md={5} className="g-4">
         <Col className="wd-dashboard-course" style={{width:"300px"}}>
         <Card>
-          <Link href="/Courses/1234" className="wd-dashboard-course-link text-decorstion-none text-dark">
+          <Link href="/Courses/1234" className="wd-dashboard-course-link text-decoration-none text-dark">
             <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160} />
             <CardBody>
-              <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden"> CS1234 React JS </CardTitle>
+              <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden"> CS1234 React JS</CardTitle>
               <CardText className="wd-dashboard-course-description overflow-hidden" style={{height:"100px"}}>
                 Full Stack software developer
               </CardText>
@@ -24,6 +23,7 @@ export default function Dashboard() {
           </Link>
           </Card>
         </Col>
+        
         <Col className="wd-dashboard-course" style={{width:"300px"}}>
         <Card>
           <Link href="/Courses/4000" className="wd-dashboard-course-link text-decoration-none text-dark">
@@ -41,7 +41,7 @@ export default function Dashboard() {
         
         <Col className="wd-dashboard-course" style={{width:"300px"}}>
           <Card>
-          <Link href="/Courses/4001" className="wd-dashboard-course-link">
+          <Link href="/Courses/4001" className="wd-dashboard-course-link text-decoration-none text-dark">
             <CardImg variant="top" src="/images/cpp.jpg" width="100%" height={160} />
             <CardBody>
               <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden"> CS4001 C++</CardTitle>
@@ -57,7 +57,7 @@ export default function Dashboard() {
 
         <Col className="wd-dashboard-course" style={{width:"300px"}}>
           <Card>
-          <Link href="/Courses/4002" className="wd-dashboard-course-link">
+          <Link href="/Courses/4002" className="wd-dashboard-course-link text-decoration-none text-dark">
             <CardImg variant="top" src="/images/cs.jpg" width="100%" height={160} />
             <CardBody>
               <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden"> CS4002 C#</CardTitle>
@@ -73,7 +73,7 @@ export default function Dashboard() {
 
         <Col className="wd-dashboard-course" style={{width:"300px"}}>
         <Card>
-          <Link href="/Courses/4003" className="wd-dashboard-course-link">
+          <Link href="/Courses/4003" className="wd-dashboard-course-link text-decoration-none text-dark">
             <CardImg variant="top" src="/images/java.jpg" width="100%" height={160} />
             <CardBody>
               <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden"> CS4003 Java</CardTitle>
@@ -88,7 +88,7 @@ export default function Dashboard() {
 
         <Col className="wd-dashboard-course" style={{width:"300px"}}>
         <Card>
-          <Link href="/Courses/4004" className="wd-dashboard-course-link">
+          <Link href="/Courses/4004" className="wd-dashboard-course-link text-decoration-none text-dark">
             <CardImg variant="top" src="/images/ruby.jpg" width="100%" height={160} />
             <CardBody>
               <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden"> CS4004 Ruby</CardTitle>
@@ -104,7 +104,7 @@ export default function Dashboard() {
 
         <Col className="wd-dashboard-course" style={{width:"300px"}}>
         <Card>
-          <Link href="/Courses/4005" className="wd-dashboard-course-link">
+          <Link href="/Courses/4005" className="wd-dashboard-course-link text-decoration-none text-dark">
             <CardImg variant="top" src="/images/swift.jpg" width="100%" height={160} />
             <CardBody>
               <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden"> CS4005 swift</CardTitle>
