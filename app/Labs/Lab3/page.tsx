20a1a7dcec3982726749f8dc9d68b7a6057ddde1
JerryYanTY <yan.tianyu@northeastern.edu>
2025-10-16 04:01:36 +0800

    Some more

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -23,6 +23,10 @@ import Spreading from "./Spreader";
 import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+
+
 
 export default function Lab3() {
   return (
@@ -53,5 +57,7 @@ export default function Lab3() {
       <Destructing/>
       <FunctionDestructing/>
       <DestructingImports/>
+      <Classes/>
+      <Styles/>
     </div>
 );}
