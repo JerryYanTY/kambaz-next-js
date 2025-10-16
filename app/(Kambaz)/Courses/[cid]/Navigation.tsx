c28936a07dcf921aa9683f637df3fac7bc20a925
JerryYanTY <yan.tianyu@northeastern.edu>
2025-10-16 18:23:34 +0800

    Course nav update

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -1,26 +1,63 @@
+"use client";
 import { NavLink } from "react-bootstrap";
+import { usePathname,useParams } from "next/navigation";
+
+
 export default function CourseNavigation() {
-  return (
-    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <NavLink href="/Courses/1234/Home" id="wd-course-home-link"
-        className= "active list-group-item border-0 ">Home</NavLink>
-      <NavLink href="/Courses/1234/Modules" id="wd-course-modules-link" 
-        className="list-group-item text-danger border-0">Modules
-      </NavLink>
-      <NavLink href="/Courses/1234/Piazza" id="wd-course-piazza-link"
-      className="list-group-item text-danger border-0">Piazza</NavLink>
-      <NavLink href="/Courses/1234/Zoom" id="wd-course-zoom-link"
-      className="list-group-item text-danger border-0">Zoom</NavLink>
-      <NavLink href="/Courses/1234/Assignments" id="wd-course-quizzes-link"
-      className="list-group-item text-danger border-0">
-        Assignments</NavLink>
-      <NavLink href="/Courses/1234/Quizzes" id="wd-course-assignments-link"
-      className="list-group-item text-danger border-0">Quizzes
-      </NavLink>
-      <NavLink href="/Courses/1234/Grades" id="wd-course-grades-link"
-      className="list-group-item text-danger border-0">Grades</NavLink>
-      <NavLink href="/Courses/1234/People/Table" id="wd-course-people-link"
-      className="list-group-item text-danger border-0">People</NavLink><br />
-    </div>
-  );
-}
+  const { cid } = useParams<{cid : string}>();
+  const pathname = usePathname();
+  const links = [
+    "Home", 
+    "Modules", 
+    "Piazza", 
+    "Zoom", 
+    "Assignments", 
+    "Quizzes", 
+    "Grades", 
+    "People"];
+    return (
+      <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+      {links.map((label) => {
+        const href = `/Courses/${cid}/${label}`;
+        const isActive = pathname.includes(label);
+
+        return (
+          <NavLink
+            key={label}
+            href={href}
+            id={`wd-course-${label}-link`}
+            className={`list-group-item list-group-item-action border-0 ${
+              isActive ? "active" : "text-danger"
+            }`}
+          >
+            {label}
+          </NavLink>
+        );
+      })}
+      </div>
+    );
+  }
+  // return (
+  //   <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+  //     <NavLink href="/Courses/1234/Home" id="wd-course-home-link"
+  //       className= "active list-group-item border-0 ">Home</NavLink>
+  //     <NavLink href="/Courses/1234/Modules" id="wd-course-modules-link" 
+  //       className="list-group-item text-danger border-0">Modules
+  //     </NavLink>
+  //     <NavLink href="/Courses/1234/Piazza" id="wd-course-piazza-link"
+  //     className="list-group-item text-danger border-0">Piazza</NavLink>
+  //     <NavLink href="/Courses/1234/Zoom" id="wd-course-zoom-link"
+  //     className="list-group-item text-danger border-0">Zoom</NavLink>
+  //     <NavLink href="/Courses/1234/Assignments" id="wd-course-quizzes-link"
+  //     className="list-group-item text-danger border-0">
+  //       Assignments</NavLink>
+  //     <NavLink href="/Courses/1234/Quizzes" id="wd-course-assignments-link"
+  //     className="list-group-item text-danger border-0">Quizzes
+  //     </NavLink>
+  //     <NavLink href="/Courses/1234/Grades" id="wd-course-grades-link"
+  //     className="list-group-item text-danger border-0">Grades</NavLink>
+  //     <NavLink href="/Courses/1234/People/Table" id="wd-course-people-link"
+  //     className="list-group-item text-danger border-0">People</NavLink><br />
+  //   </div>
+  // );
+// }
--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -6,20 +6,19 @@ import { courses } from "../../Database";
 export default async function CoursesLayout({
   children,
   params,
-}: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
-  const { cid } = await params;
+}: Readonly<{ children: ReactNode; params: { cid: string } }>) {
+  const { cid } = params;
   const course = courses.find((course) => course._id === cid);
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
-        {" "}
         <FaAlignJustify className="me-4 fs-4 mb-1"></FaAlignJustify>
         {course?.name}
       </h2>
       <hr />
       <div className="d-flex">
         <div className="d-none d-md-block">
-          <CourseNavigation />{" "}
+          <CourseNavigation  />
         </div>
         <div className="flex-fill"> {children} </div>
       </div>
