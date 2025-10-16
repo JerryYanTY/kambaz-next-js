869cbd9645e6596c13122a5752aecbd638e17246
JerryYanTY <yan.tianyu@northeastern.edu>
2025-10-17 00:26:59 +0800

    Update page.tsx

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/Editor/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/Editor/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { useParams } from "next/navigation";
 import AssignmentEditor from "./Editor";
 import  Button  from "react-bootstrap/Button";
 export default function Editor() {
+    const {cid} = useParams();
+    const backHref = `/Courses/${cid}/Assignments`;
     return (
    <div id ="wd-editor">
     <h1>Assignment Editor</h1>
@@ -8,8 +12,8 @@ export default function Editor() {
         <hr className="my-4">
         </hr>
         <div className="d-flex justify-content-end gap-2">
-            <Button variant="light">Cancel</Button>
-            <Button variant="danger">Save</Button>
+            <Button variant="light" href ={backHref}>Cancel</Button>
+            <Button variant="danger"href ={backHref}>Save</Button>
         </div>
    </div>
 
