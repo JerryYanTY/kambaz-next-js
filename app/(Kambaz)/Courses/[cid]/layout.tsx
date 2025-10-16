936c3278ff3123d6c0542296f95cb863142c16a0
JerryYanTY <yan.tianyu@northeastern.edu>
2025-10-16 17:36:20 +0800

    Course screen

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,17 +1,28 @@
 import { ReactNode } from "react";
 import { FaAlignJustify } from "react-icons/fa";
 import CourseNavigation from "./Navigation";
-export default async function CoursesLayout(
-  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
- const { cid } = await params;
- return (
-   <div id="wd-courses">
-     <h2 className="text-danger"> <FaAlignJustify className="me-4 fs-4 mb-1"></FaAlignJustify>Courses {cid}</h2>
-     <hr />
-     <div className="d-flex">
-      <div className="d-none d-md-block"> 
-         <CourseNavigation /> </div>
-           <div className="flex-fill"> {children} </div>
-         </div>
-       </div>
-);}
+import { courses } from "../../Database";
+
+export default async function CoursesLayout({
+  children,
+  params,
+}: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
+  const { cid } = await params;
+  const course = courses.find((course) => course._id === cid);
+  return (
+    <div id="wd-courses">
+      <h2 className="text-danger">
+        {" "}
+        <FaAlignJustify className="me-4 fs-4 mb-1"></FaAlignJustify>
+        {course?.name}
+      </h2>
+      <hr />
+      <div className="d-flex">
+        <div className="d-none d-md-block">
+          <CourseNavigation />{" "}
+        </div>
+        <div className="flex-fill"> {children} </div>
+      </div>
+    </div>
+  );
+}
