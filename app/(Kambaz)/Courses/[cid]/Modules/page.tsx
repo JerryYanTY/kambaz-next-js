255e8196bb06fc205fcc6caf548b0835c93a2a0c
JerryYanTY <yan.tianyu@northeastern.edu>
2025-10-16 18:57:19 +0800

    Modules

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,18 +1,55 @@
+"use client";
 import ModulesControls from "./ModulesControls";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import LessonControlButtons from "./LessonControlButtons";
 import { BsGripVertical } from "react-icons/bs";
 import ModuleControlButtons from "./ModuleControlButtons";
+import { useParams } from "next/navigation";
+import * as db from "../../../Database";
+
 export default function Modules() {
+  const { cid } = useParams();
+  const modules = db.modules;
   return (
-
-
     <div>
-
       <ModulesControls />
-      <br /> <br /><br /><br />
-      {/* Implement Collapse All button, View Progress button, etc. */}
+      <br /> <br />
+      <br />
+      <br />
       <ListGroup id="wd-modules" className="rounded-0">
+        {modules
+          .filter((module: any) => module.course === cid)
+          .map((module: any) => (
+            <ListGroupItem 
+            key = {module.id}
+            className="wd-module p-0 mb-5 fs-5 border-gray">
+              <div className="wd-title p-3 ps-2 bg-secondary">
+                <BsGripVertical className="me-2 fs-3" /> {module.name}{" "}
+                <ModuleControlButtons />
+              </div>
+              {module.lessons && (
+                <ListGroup className="wd-lessons rounded-0">
+                  {module.lessons.map((lesson: any) => (
+                    <ListGroupItem className="wd-lesson p-3 ps-1"
+                    key={lesson.id}>
+                      <BsGripVertical className="me-2 fs-3" /> {lesson.name}{" "}
+                      <LessonControlButtons />
+                    </ListGroupItem>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroupItem>
+          ))}
+      </ListGroup>
+    </div>
+  );
+}
+
+{
+  /* Implement Collapse All button, View Progress button, etc. */
+}
+{
+  /* <ListGroup id="wd-modules" className="rounded-0">
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary">
             <BsGripVertical className="me-2 fs-3"/> 
@@ -88,4 +125,5 @@ export default function Modules() {
           </ListGroup>
         </div >
   );
+} */
 }
