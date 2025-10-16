4f6e9f4428216bf6fc82933c776dd398a06fb01e
JerryYanTY <yan.tianyu@northeastern.edu>
2025-10-16 17:26:29 +0800

    Dashboard

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,15 +1,54 @@
 import Link from "next/link";
 import { Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import {CardBody, CardImg,CardText, CardTitle, Card} from "react-bootstrap";
+import * as db from "../Database";
+import { CardBody, CardImg, CardText, CardTitle, Card } from "react-bootstrap";
 export default function Dashboard() {
+  const courses = db.courses;
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
+      <h2 id="wd-dashboard-published">
+        Published Courses ({courses.length})
+      </h2>
+      <hr />
       <div id="wd-dashboard-courses">
-        <Row xs={1} md={5} className="g-4 gy-4">
-        <Col className="wd-dashboard-course" style={{width:"300px"}}>
+        <Row xs={1} md={5} className="g-4">
+          {courses.map((course) => (
+            <Col key ={course._id} className="wd-dashboard-course" style={{ width : "300px" }}>
+              <Card>
+                <Link
+                  href={`/Courses/${course._id}/Home`}
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    src={course.image}
+                    variant="top"
+                    width="100%"
+                    height={160}
+                  />
+                  <CardBody className="card-body">
+                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      {course.name}{" "}
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      {course.description}{" "}
+                    </CardText>
+                    <Button variant="primary"> Go </Button>
+                  </CardBody>
+                </Link>
+              </Card>
+            </Col>
+          ))}
+                  </Row>
+      </div>
+    </div>
+  );
+}
+          {/* <Col className="wd-dashboard-course" style={{width:"300px"}}>
         <Card>
           <Link href="/Courses/1234" className="wd-dashboard-course-link text-decoration-none text-dark">
             <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160} />
@@ -115,8 +154,5 @@ export default function Dashboard() {
             </CardBody>
           </Link>
           </Card>
-        </Col>
-        </Row>
-      </div>
-    </div>
-);}
+        </Col> */}
+
