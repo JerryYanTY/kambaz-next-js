5318401097437f06f897f802eff272b86491a822
JerryYanTY <yan.tianyu@northeastern.edu>
2025-10-31 20:13:37 +0800

    4.3.4

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,5 +1,7 @@
 import HelloRedux from "./HelloRedux/page";
 import CounterRedux from "./CouterRedux/page";
+import AddRedux from "./AddRedux/page";
+
 
 export default function ReduxExamples() {
     return (
@@ -7,6 +9,7 @@ export default function ReduxExamples() {
             <h2>Redux Examples</h2>
             <HelloRedux/>
             <CounterRedux/>
+            <AddRedux/>
         </div>
     );
 };
