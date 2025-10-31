c58951e9f9cae58639ad547b5df4cc20a4f6cbf2
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-01 00:06:04 +0800

    A few more bugs

--- app/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "./todosReducer";
 
 
-export default function TodoItem({todo,}) {
+export default function TodoItem({todo}:{todo:any}) {
     const dispatch = useDispatch();
   return (
     <ListGroupItem key={todo.id}
