7654e74243349c4dc7086775e90e27463c51c5c8
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-04 21:24:47 +0800

    Edit button

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { FormControl } from "react-bootstrap";
-import React, { useState} from "react";
-import {v4 as uuidv4} from "uuid";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Link from "next/link";
 import { Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
@@ -9,44 +9,60 @@ import * as db from "../Database";
 import { CardBody, CardImg, CardText, CardTitle, Card } from "react-bootstrap";
 export default function Dashboard() {
   const [courses, setCourses] = useState<any[]>(db.courses);
-  const [course, setCourse]= useState<any>({
-    _id: "0", name: "new course", number: "new number",
-    startDate: "2023-09-29",endDate: "2023-12-15",
-    department: "Some Department", credits: "4",
-    image: "/images/reactjs.jpg", description: "Some description"
+  const [course, setCourse] = useState<any>({
+    _id: "0",
+    name: "new course",
+    number: "new number",
+    startDate: "2023-09-29",
+    endDate: "2023-12-15",
+    department: "Some Department",
+    credits: "4",
+    image: "/images/reactjs.jpg",
+    description: "Some description",
   });
-  const addNewCourse=()=>{
-    const newCourse = {...course, _id: uuidv4()};
-    setCourses([...courses,newCourse]);
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: uuidv4() };
+    setCourses([...courses, newCourse]);
   };
   const deleteCourse = (courseId: string) => {
-    setCourse(courses.filter((course)=>course._id !== courseId));
+    setCourse(courses.filter((course) => course._id !== courseId));
   };
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h5>New Course
-        <Button className="btn btn-primary float-end"
-                id = "wd-add-new-course-click"
-                onClick={addNewCourse}>Add
+      <h5>
+        New Course
+        <Button
+          className="btn btn-primary float-end"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse}
+        >
+          Add
         </Button>
       </h5>
-      <br/>
-      <FormControl value={course.name ?? ""} className="mb-2"
-      onChange={(e)=>setCourse({...course, name:e.target.value})}/>
-
-      <FormControl value={course.description} as="textarea" rows={3}
-      onChange={(e)=>setCourse({...course, description: e.target.value})}/>
-      <hr/>
-
-      <h2 id="wd-dashboard-published">
-        Published Courses ({courses.length})
-      </h2>
+      <br />
+      <FormControl
+        value={course.name ?? ""}
+        className="mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <FormControl
+        value={course.description}
+        as="textarea"
+        rows={3}
+        onChange={(e) => setCourse({ ...course, description: e.target.value })}
+      />
+      <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
       <hr />
       <div id="wd-dashboard-courses">
         <Row xs={1} md={5} className="g-4">
           {courses.map((course) => (
-            <Col key ={course._id} className="wd-dashboard-course" style={{ width : "300px" }}>
+            <Col
+              key={course._id}
+              className="wd-dashboard-course"
+              style={{ width: "300px" }}
+            >
               <Card>
                 <Link
                   href={`/Courses/${course._id}/Home`}
@@ -69,25 +85,40 @@ export default function Dashboard() {
                       {course.description}{" "}
                     </CardText>
                     <Button variant="primary"> Go </Button>
-                    <Button onClick={(event)=>{
-                      event.preventDefault;
-                      deleteCourse(course._id);
-                    }}
-                    className="btn btn-danger float-end"
-                    id = "wd-delete-course-click">
-                    Delete
+                    
+
+                    <Button
+                      onClick={(event) => {
+                        event.preventDefault;
+                        deleteCourse(course._id);
+                      }}
+                      className="btn btn-danger float-end"
+                      id="wd-delete-course-click"
+                    >
+                      Delete
+                    </Button>
+                    <Button
+                      id="wd-edit-course-click"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}
+                      className="btn btn-warning me-2 float-end"
+                    >
+                      Edit
                     </Button>
                   </CardBody>
                 </Link>
               </Card>
             </Col>
           ))}
-                  </Row>
+        </Row>
       </div>
     </div>
   );
 }
-          {/* <Col className="wd-dashboard-course" style={{width:"300px"}}>
+{
+  /* <Col className="wd-dashboard-course" style={{width:"300px"}}>
         <Card>
           <Link href="/Courses/1234" className="wd-dashboard-course-link text-decoration-none text-dark">
             <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160} />
@@ -193,5 +224,5 @@ export default function Dashboard() {
             </CardBody>
           </Link>
           </Card>
-        </Col> */}
-
+        </Col> */
+}
