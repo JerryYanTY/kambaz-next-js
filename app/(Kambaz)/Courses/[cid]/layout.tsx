b73103aeb4a0e3aedfa116777092050dd9d16976
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-05 03:21:43 +0800

    4.4.2.1

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { FaAlignJustify } from "react-icons/fa";
 import CourseNavigation from "./Navigation";
 import { courses } from "../../Database";
@@ -7,7 +7,7 @@ import Breadcrumb from "./Breadcrumb";
 import { useParams } from "next/navigation";
 import { useSelector } from "react-redux";
 import { RootState } from "@reduxjs/toolkit/query";
-
+import { Button } from "react-bootstrap";
 
 export default function CoursesLayout(
   {children}: {children: ReactNode
@@ -15,19 +15,33 @@ export default function CoursesLayout(
   const { cid } =  useParams();
   const {courses} = useSelector((state: RootState) => state.coursesReducer);
   const course = courses.find((course: any)=> course._id === cid);
+  const [showNav, setShowNav] = useState(true);
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
-        <FaAlignJustify className="me-4 fs-4 mb-1"></FaAlignJustify>
+        {/* <FaAlignJustify className="me-4 fs-4 mb-1"></FaAlignJustify> */}
+        <Button
+        type="button"
+        className="btn btn-link p-0 me-3 align-middle"
+        aria-label="Toggle course nav"
+        aria-pressed={showNav}
+        onClick={()=>setShowNav((s) => !s)}>
+          <FaAlignJustify className="fs-4 text-danger"/>
+        </Button>
         {/* <Breadcrumb course = {course}/> */}
         {course?.name}
       </h2>
 
       <hr />
       <div className="d-flex">
-        <div className="d-none d-md-block">
+        {/* <div className="d-none d-md-block">
           <CourseNavigation  />
-        </div>
+        </div> */}
+        {showNav && (
+          <div className="d-none d-md-block">
+            <CourseNavigation/>
+            </div>
+        )}
         <div className="flex-fill"> {children} </div>
       </div>
     </div>
