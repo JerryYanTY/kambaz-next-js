432a86c283b2b4fa70fdcba491675c4e53ecae4e
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-06 02:34:11 +0800

    Editing module

--- app/(Kambaz)/Courses/[cid]/Modules/ModuleControlButtons.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/ModuleControlButtons.tsx
@@ -2,16 +2,20 @@ import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "../Assignments/GreenCheckmark";
 import { BsPlus } from "react-icons/bs";
 import { FaTrash } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
 
 export default function ModuleControlButtons(
-  {moduleId, deleteModule} :
+  {moduleId, deleteModule, editModule} :
   { moduleId: string;
     deleteModule: (moduleId: string) => void;
+    editModule: (moduleId: string)=>void;
   }
 ) {
 
   return (
     <div className="float-end">
+      <FaPencil className="text-primary me-3"
+      onClick={() => editModule(moduleId)} />
       <FaTrash className="text-danger me-2 mb-1" 
       onClick={() => deleteModule(moduleId)}/>
       <GreenCheckmark />
--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -8,6 +8,8 @@ import { BsGripVertical } from "react-icons/bs";
 import ModuleControlButtons from "./ModuleControlButtons";
 import { useParams } from "next/navigation";
 import * as db from "../../../Database";
+import { FormControl } from "react-bootstrap";
+
 
 export default function Modules() {
   const [modules, setModules] = useState<any[]>(db.modules);
@@ -16,10 +18,17 @@ export default function Modules() {
   const addModule = () =>{
     setModules([...modules, {_id: uuidv4(), name: moduleName, course: cid, lessons: []}]);
     setModuleName("");
-  }
+  };
   const deleteModule = (moduleId: string) => {
     setModules(modules.filter((m)=>m._id!== moduleId));
-  }
+  };
+  const editModule = (moduleId: string) =>{
+    setModules(modules.map((m) => (m._id === moduleId ? {...m, editing: true}: m)))
+  };
+  const updateModule = (module: any) => {
+    setModules(modules.map((m) => (m._id === module._id ? module: m)));
+  };
+
   return (
     <div>
       <ModulesControls setModuleName={setModuleName}
@@ -36,10 +45,22 @@ export default function Modules() {
             key = {module._id}
             className="wd-module p-0 mb-5 fs-5 border-gray">
               <div className="wd-title p-3 ps-2 bg-secondary">
-                <BsGripVertical className="me-2 fs-3" /> {module.name}{" "}
+                <BsGripVertical className="me-2 fs-3" /> 
+                {!module.editing && module.name}
+                {module.editing && (
+                  <FormControl className="w-50 d-inline-block"
+                  onChange={(e) => updateModule({ ... module, name: e.target.value})}
+                  onKeyDown={(e)=> {
+                    if (e.key == "Enter") {
+                      updateModule({...module, editing:false});
+                    }
+                  }}
+                  defaultValue={module.name}/>
+                )}
                 <ModuleControlButtons 
                 moduleId={module._id}
-                deleteModule={deleteModule}/>
+                deleteModule={deleteModule}
+                editModule={editModule}/>
               </div>
               {module.lessons && (
                 <ListGroup className="wd-lessons rounded-0">
