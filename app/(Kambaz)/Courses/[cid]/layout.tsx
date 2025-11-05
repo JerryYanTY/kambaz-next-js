31e696045762e1cd0088bd7597ab156c9cca7960
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-05 21:38:37 +0800

    Style a bit

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -7,25 +7,29 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { Button } from "react-bootstrap";
 
-export default function CoursesLayout(
-  {children}: {children: ReactNode
-}) {
-  const { cid } =  useParams();
-  const {courses} = useSelector((state: RootState) => state.coursesReducer);
-  const course = courses.find((course: any)=> course._id === cid);
+export default function CoursesLayout({ children }: { children: ReactNode }) {
+  const { cid } = useParams();
+  const { courses } = useSelector((state: RootState) => state.coursesReducer);
+  const course = courses.find((course: any) => course._id === cid);
   const [showNav, setShowNav] = useState(true);
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         {/* <FaAlignJustify className="me-4 fs-4 mb-1"></FaAlignJustify> */}
-        <Button
+        {/* <Button
         type="button"
-        className="btn btn-link p-0 me-3 align-middle"
+        className="btn btn-link p-0 me-3 align-middle no-hover"
         aria-label="Toggle course nav"
         aria-pressed={showNav}
         onClick={()=>setShowNav((s) => !s)}>
           <FaAlignJustify className="fs-4 text-danger"/>
-        </Button>
+        </Button> */}
+        <FaAlignJustify
+          className="fs-4 me-3 align-middle"
+          style={{ cursor: "pointer" }}
+          onClick={() => setShowNav((s) => !s)}
+        />
+
         {/* <Breadcrumb course = {course}/> */}
         {course?.name}
       </h2>
@@ -37,8 +41,8 @@ export default function CoursesLayout(
         </div> */}
         {showNav && (
           <div className="d-none d-md-block">
-            <CourseNavigation/>
-            </div>
+            <CourseNavigation />
+          </div>
         )}
         <div className="flex-fill"> {children} </div>
       </div>
