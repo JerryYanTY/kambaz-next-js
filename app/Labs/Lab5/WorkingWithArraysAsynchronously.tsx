94fbb62ad316faa64ed673595567dae619ea7805
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-17 03:47:04 +0800

    5.2.6.3

--- app/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ app/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -4,12 +4,32 @@ import React, { useState, useEffect } from "react";
 import * as client from "./client";
 import { FaTrash } from "react-icons/fa";
 import { FaPlusCircle } from "react-icons/fa";
+import { TiDelete } from "react-icons/ti";
+import { FaPencil } from "react-icons/fa6";
+import { FormControl } from "react-bootstrap";
 
 export default function WorkingWithArraysAsynchronously() {
   const [todos, setTodos] = useState<any[]>([]);
   const createNewTodo = async () => {
     const todos = await client.createNewTodo();
     setTodos(todos);
+  };
+  const postNewTodo = async () => {
+    const newTodo = await client.postNewTodo({
+      title: "New Posted Todo",
+      completed: false,
+    });
+    setTodos([...todos, newTodo]);
+  };
+  const editTodo = (todo: any) => {
+    const updatedTodos = todos.map((t) =>
+      t.id === todo.id ? { ...todo, editing: true } : t
+    );
+    setTodos(updatedTodos);
+  };
+  const updateTodo = async (todo: any) => {
+    await client.updateTodo(todo);
+    setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
   };
 
   const fetchTodos = async () => {
@@ -19,6 +39,11 @@ export default function WorkingWithArraysAsynchronously() {
   const removeTodo = async (todo: any) => {
     const updatedTodos = await client.removeTodo(todo);
     setTodos(updatedTodos);
+  };
+  const deleteTodo = async (todo: any) => {
+    await client.deleteTodo(todo);
+    const newTodos = todos.filter((t) => t.id !== todo.id);
+    setTodos(newTodos);
   };
 
   useEffect(() => {
@@ -34,20 +59,47 @@ export default function WorkingWithArraysAsynchronously() {
           onClick={createNewTodo}
           className="text-success float-end fs-3"
         />
+        <FaPlusCircle
+          onClick={postNewTodo}
+          className="text-primary float-end fs-3 me-3"
+          id="wd-post-todo"
+        />
       </h4>
       <ListGroup>
         {todos.map((todo) => (
           <ListGroupItem key={todo.id}>
+            <FaPencil
+              onClick={() => editTodo(todo)}
+              className="text-primary float-end me-2 mt-1"
+            />
             <FaTrash
               onClick={() => removeTodo(todo)}
               className="text-danger float-end mt-1"
               id="wd-remove-todo"
             />
+            <TiDelete
+              onClick={() => deleteTodo(todo)}
+              className="text-danger float-end me-2 fs-3"
+              id="wd-delete-todo"
+            />
             <input
               type="checkbox"
               className="form-check-input me-2"
               defaultChecked={todo.completed}
+              onChange={(e) => updateTodo({ ...todo, completed: e.target.checked }) } 
             />
+            {!todo.editing ? ( todo.title ) : (
+                <FormControl className="w-50 float-start" defaultValue={todo.title}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      updateTodo({ ...todo, editing: false });
+                    }
+                  }}
+                  onChange={(e) =>
+                    updateTodo({ ...todo, title: e.target.value })
+                  }
+                />
+              )}
             <span
               style={{
                 textDecoration: todo.completed ? "line-through" : "none",
