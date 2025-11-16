bc32c5f3370229dd94456ecb775f4ad17d878c1e
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-17 02:04:09 +0800

    5.2.2.4

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -2,6 +2,7 @@ import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
 
 
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
@@ -19,6 +20,7 @@ export default function Lab5() {
         <PathParameters/>
         <QueryParameters/>
         <WorkingWithObjects/>
+        <WorkingWithArrays />
       </div>
   );}
   
