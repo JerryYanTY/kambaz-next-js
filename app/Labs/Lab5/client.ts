3281a50a3f72a0e6702e565107d8a77a42247675
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-17 03:10:45 +0800

    5.2.5.7

--- app/Labs/Lab5/client.ts
+++ app/Labs/Lab5/client.ts
@@ -4,3 +4,22 @@ export const fetchWelcomeMessage = async () => {
   const response = await axios.get(`${HTTP_SERVER}/lab5/welcome`);
   return response.data;
 };
+const ASSIGNMENT_API = `${HTTP_SERVER}/lab5/assignment`;
+export const fetchAssignment = async () => {
+  const response = await axios.get(`${ASSIGNMENT_API}`);
+  return response.data;
+};
+export const updateTitle = async (title: string) => {
+  const response = await axios.get(`${ASSIGNMENT_API}/title/${title}`);
+  return response.data;
+};
+const TODOS_API = `${HTTP_SERVER}/lab5/todos`;
+export const fetchTodos = async () => {
+  const response = await axios.get(TODOS_API);
+  return response.data;
+};
+export const removeTodo = async (todo: any) => {
+    const response = await axios.get(`${TODOS_API}/${todo.id}/delete`);
+    return response.data;
+  };
+  
--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -4,6 +4,9 @@ import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithArrays from "./WorkingWithArrays";
 import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+
 
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export default function Lab5() {
@@ -22,6 +25,8 @@ export default function Lab5() {
         <WorkingWithObjects/>
         <WorkingWithArrays />
         <HttpClient/>
+        <WorkingWithObjectsAsynchronously/>
+        <WorkingWithArraysAsynchronously />
       </div>
   );}
   
