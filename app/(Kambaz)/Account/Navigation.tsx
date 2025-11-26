484d52fcff5169f44eea9a598c015290ed9500aa
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-27 01:57:33 +0800

    6.2.6.3

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -24,5 +24,7 @@ export default function AccountNavigation() {
       >
         {link.label} </NavLink> </NavItem>
   ))}
+       {currentUser && currentUser.role === "ADMIN" && (
+       <NavLink as={Link} href={`/Account/Users`}  active={pathname.endsWith('Users')}> Users </NavLink> )}
 </Nav>
 );}
